c1d88852cb051f120e5588bbb922ca2226f6cfa2
Shaik Rehan <abacusrehan@gmail.com>
2021-09-21 08:35:58 +0000

    Initial Commit

--- src/components/imageDetail.js
+++ src/components/imageDetail.js
@@ -1,20 +1,14 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import tw from 'tailwind-react-native-classnames';
 const ImageDetail = ({ title, imageURL }) => {
     return (
         <View>
             <Text>{title}</Text>
-            <img src={imageURL} alt={title} style={tw`w-20 h-[20px]`} />
+            <img src={imageURL} alt={title} />
         </View>
     )
 }
 
 export default ImageDetail
 
-const styles = StyleSheet.create({
-    image_djkd: {
-        width: 100,
-        height: 100,
-    }
-})
+const styles = StyleSheet.create({})
